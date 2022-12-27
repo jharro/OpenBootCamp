00df30eb70b215c4e747725782b53a93bf175437
jharro <jharro@gmail.com>
2022-12-27 12:20:15 -0500

    Ejercicios del Tema 1 del curso de Java Básico
    Ejercicios del Tema 1 del curso de Java Básico

---
+++ src/funciones/EjercicioTema1.java
@@ -0,0 +1,24 @@
+package cursojavabasico;
+
+public class EjercicioTema1 {
+    public static void main(String[] args) {
+        byte bite=100;
+        int entero=56000;
+        short corto=31700;
+        long grande=456900;
+        float flotante=45.83F;
+        double doble=89581245.8345;
+        boolean logico=true;
+        String cadena="Soy de Colombia";
+        System.out.println("Impresión de cada dato");
+        System.out.println("BYTE: "+bite);
+        System.out.println("INT: "+entero);
+        System.out.println("SHORT: "+corto);
+        System.out.println("LONG: "+grande);
+        System.out.println("FLOAT: "+flotante);
+        System.out.println("DOUBLE: "+doble);
+        System.out.println("BOOLEAN: "+logico);
+        System.out.println("STRING: "+cadena);
+
+    }
+}
